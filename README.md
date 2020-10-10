132b886c94f923a71964aaeb4008110d1cb343cd
MartinLedel <martinledel@gmail.com>
2020-10-10 16:05:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Setup
+
+1. Clone the repo with `git clone https://github.com/MartinLedel/jsramverk-socket`
+2. Run `npm install` to install all dependencies
+3. Run `npm run start`
+4. It now runs on `http://localhost:8001/`
---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+function chatLimit(chat, data) {
+    if (chat.length >= 5) {
+        console.log(chat[0]);
+        chat.splice(0, 1, data);
+    } else {
+        chat.push(data);
+    }
+
+    return chat;
+}
+
+io.on('connection', function(socket) {
+    let chatArr = [];
+
+    socket.on('user connecting', function(user) {
+        console.info('user connected', user);
+        let data = "[" + user.time + " User " + user.user + " connected]";
+
+        chatArr = chatLimit(chatArr, data);
+        io.emit('user broadcast', chatArr);
+    });
+
+    socket.on('chat message', function (message) {
+        console.info('chat message', message);
+        let data = "[" + message.time + "] " + message.user + ": " + message.message;
+
+        chatArr = chatLimit(chatArr, data);
+        io.emit('chat message', chatArr);
+    });
+});
+
+server.listen(8001);
+
+// io.origins(['https://socket-client.jsramverk.me:443']);
+// io.origins(['https://localhost:8001']);
+// io.set('origins', '*:*');
