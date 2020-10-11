2b2a68304eacae9c273cdf91c3781f5e7fce84d8
MartinLedel <martinledel@gmail.com>
2020-10-11 22:17:39 +0200

    cors fix part 6

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-io.origins(['https://ml-jsramverk.me/:443']);
+io.origins(['https://ml-jsramverk.me:443']);
 
 function chatLimit(chat, data) {
     if (chat.length >= 5) {
