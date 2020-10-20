b72b270de03b1752533b490e2019114ba7aff41d
MartinLedel <martinledel@gmail.com>
2020-10-20 12:31:07 +0200

    It now saves messages to monogodb

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
 const express = require('express');
 const app = express();
 
+const mongo = require('mongodb').MongoClient;
+const dsn = 'mongodb://localhost:27017';
+const colName = 'history';
+
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-io.origins(['https://ml-jsramverk.me:443']);
+// io.origins(['https://ml-jsramverk.me:443']);
 
-function chatLimit(chat, data) {
-    if (chat.length >= 5) {
-        console.log(chat[0]);
-        chat.splice(0, 1, data);
-    } else {
-        chat.push(data);
-    }
-
-    return chat;
+async function insertToCollection(msg) {
+    const client  = await mongo.connect(dsn);
+    const db = await client.db();
+    const col = await db.collection(colName);
+    const result = await col.insertOne({ message: msg });
+    console.log(
+      `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
+    );
+    await client.close();
 }
 
-io.on('connection', function(socket) {
+io.on('connection', async function(socket) {
     let chatArr = [];
 
-    socket.on('user connecting', function(user) {
+    socket.on('user connecting', async function(user) {
         console.info('user connected', user);
-        let data = "[" + user.time + " User " + user.user + " connected]";
+        let data = `[${user.time}] User ${user.user} has connected`;
 
-        chatArr = chatLimit(chatArr, data);
+        chatArr.push(data);
         io.emit('user broadcast', chatArr);
+        await insertToCollection(data).catch(console.dir);
     });
 
-    socket.on('chat message', function (message) {
+    socket.on('chat message', async function (message) {
         console.info('chat message', message);
-        let data = "[" + message.time + "] " + message.user + ": " + message.message;
+        let data = `[${message.time}] ${message.user}: ${message.message}`;
 
-        chatArr = chatLimit(chatArr, data);
+        chatArr.push(data);
         io.emit('chat message', chatArr);
+        await insertToCollection(data).catch(console.dir);
     });
 });
 
